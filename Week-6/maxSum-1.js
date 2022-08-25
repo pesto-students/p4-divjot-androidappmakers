fe76a6c804479a8c135b14ed929a9eb59955d648
gopalkrishan <gopalkrishan.1997@gmail.com>
2022-08-25 11:51:00 +0530

    added implementation for Assignment 1,2,3

---
+++ Week-6/maxSum-1.js
@@ -0,0 +1,13 @@
+//Kadane's Algo
+var maxSubArray = function(nums) {
+  var maxSum=nums[0];
+  var currentSum=maxSum;
+  for(var i=1;i<nums.length;i++){
+      currentSum=Math.max(nums[i]+currentSum,nums[i]);
+      maxSum=Math.max(currentSum,maxSum);
+  }
+  return maxSum;
+
+};
+
+console.log(maxSubArray([4,-1,2,1]));
---
+++ Week-6/sort.js
@@ -0,0 +1,27 @@
+ var sortColors = function(nums) {
+    if(nums.length==0||nums.length==1)return;
+    
+    var start =0;
+    var end=nums.length-1;
+    var index=0;
+    
+    while(index<=end && start <end){
+        if(nums[index]==0){
+            nums[index]=nums[start];
+            nums[start]=0;
+            index++;
+            start++;
+        }
+        else if(nums[index]==2){
+            nums[index]=nums[end];
+            nums[end]=2;
+            end--;
+        }
+        else{
+            index++;
+        }
+    }
+    console.log(nums);
+};
+
+sortColors([0,2,1,2,0]);
