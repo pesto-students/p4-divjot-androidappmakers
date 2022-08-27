517159e80392665ebf859aba2ff85a1f00fdb5cd
gopalkrishan <gopalkrishan.1997@gmail.com>
2022-08-27 19:20:51 +0530

    Assignment 4,5,6

---
+++ Week-6/difference.js
@@ -0,0 +1,35 @@
+// function findPair(arr, n) {
+//   let i = 0;
+//   let j = 1;
+//   let newarr = arr.sort();
+//   let size = arr.length;
+//   while (i < size && j < size) {
+//     console.log("1");
+//     if (i != j && newarr[j] - newarr[i] == Math.abs(n)) {
+//       console.log("reached here");
+//       return true;
+//     } else if (newarr[j] - newarr[i] < n) j++;
+//     else i++;
+//   }
+
+//   return false;
+// }
+
+
+var findPair = function(nums, k) {
+    const map = new Map();
+    for(let i of nums){
+        let val = map.has(i) ? map.get(i) : 0;
+        map.set(i, val+1);
+    }
+    for(let i of map.keys()){
+        if((k == 0 && map.get(i) > 1) || (k > 0 && map.has(i+k))){
+            return 1;
+        }
+    }
+    return 0;
+};
+
+
+
+console.log(findPair([5, 10, 3, 2, 50, 80],78));
